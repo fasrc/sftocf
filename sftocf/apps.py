d6ccb6cac0dfaf96fe4b552d2da81269c8acb77d
Claire Peters <clairepeters@g.harvard.edu>
2026-03-25 09:42:09 -0700

    fix signals import in apps.py

--- sftocf/apps.py
+++ sftocf/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
 
-from coldfront.core.utils.common import import_from_settings
-
-SFUSER = import_from_settings('SFUSER')
-SFPASS = import_from_settings('SFPASS')
-
 
 class StarFishConfig(AppConfig):
     name = 'sftocf'
     default_auto_field = 'django.db.models.BigAutoField'
-    import sftocf.signals  # noqa: F401
+
+    def ready(self):
+        import sftocf.signals  # noqa: F401
